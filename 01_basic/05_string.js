7dd34cf40df8c52d00eb04f1f2dbeed5903dfe10
simarjandir <145894355+simarjandir@users.noreply.github.com>
2023-09-24 17:54:28 +0000

    String Done

---
+++ 01_basic/05_string.js
@@ -0,0 +1,32 @@
+const name ="Simarjit";
+const repoCount=1;
+//console.log(` ${name} has ${repoCount} repositry in GitHub`);
+
+const myName = new String('Simarjit Singh');
+// console.log(myName[0]);
+// console.log(myName.length);
+// console.log(myName.toUpperCase());
+// console.log(myName.charAt(6));
+//console.log(myName.indexOf('j'));
+
+// const newString= myName.substring(1,5);
+// console.log(newString);
+
+// const anotherString = myName.slice(0,4);
+// console.log(anotherString);
+// let sliced = myName.slice(-14,10);
+// console.log(sliced);
+
+// let anoString = "     Simarjit-Singh      ";
+// console.log(anoString);
+// console.log(anoString.trim());
+
+const url="www.google.com/simarjit%20cbfbfnd";
+//console.log(url.replace('%20','-'));
+//console.log(url.includes('si'));
+
+let splitDemo="Simarjit-Singh-Jandir";
+//console.log(splitDemo.split('-'));
+
+//console.log(splitDemo.charCodeAt(3));
+console.log(String.fromCharCode(123,150,45,65));
