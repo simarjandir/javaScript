4bf93ffcecd9ba21d6cb5bfc5c87febc83df3590
simarjandir <145894355+simarjandir@users.noreply.github.com>
2023-09-24 12:42:34 +0000

    data types

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,14 @@
+let name="Simarjit";
+let age=37;
+let isLoggedIn=false;
+
+// number
+// bigint
+// string
+// boolean ==> true / false
+// null   ==> standalone value
+// undefined
+// symbol
+
+// object
+
