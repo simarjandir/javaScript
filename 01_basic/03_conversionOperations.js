e2305a303662164ca629a147305f242902009bd8
simarjandir <145894355+simarjandir@users.noreply.github.com>
2023-09-24 14:20:03 +0000

    operations

--- 01_basic/03_conversionOperations.js
+++ 01_basic/03_conversionOperations.js
@@ -1,43 +1,72 @@
-let score=3;
-let score1="33"
-let score2="33abc"
+// let score=3;
+// let score1="33"
+// let score2="33abc"
 
-console.log(typeof score);
-console.log(typeof(score1));
-console.log(typeof score2);
+// console.log(typeof score);
+// console.log(typeof(score1));
+// console.log(typeof score2);
 
-let valueInNumber = Number(score1)
-let valueInNumber2=Number(score2);
-console.log(typeof valueInNumber2);
-console.log(valueInNumber2);
+// let valueInNumber = Number(score1)
+// let valueInNumber2=Number(score2);
+// console.log(typeof valueInNumber2);
+// console.log(valueInNumber2);
 
-console.log('-------------------------------');
-let nullValue=null;
-let undefinedValue  = undefined;
-let booleanValue = true;
-console.log(typeof nullValue);
-console.log(typeof undefinedValue);
-console.log(typeof booleanValue);
-
-
-// "33" => 33
-// "33abc" => NaN
-// true =>1   false => 0
-
-console.log("----------------------------");
-
-let isLoggedIn =0;
-let booleanIsLoggedIn=Boolean(isLoggedIn);
-console.log(typeof booleanIsLoggedIn);
-
-// 1 => true     0=> false
-//""  => false
-//"Simarjit"  => true
+// console.log('-------------------------------');
+// let nullValue=null;
+// let undefinedValue  = undefined;
+// let booleanValue = true;
+// console.log(typeof nullValue);
+// console.log(typeof undefinedValue);
+// console.log(typeof booleanValue);
 
 
-console.log("-----------------------");
+// // "33" => 33
+// // "33abc" => NaN
+// // true =>1   false => 0
 
-let someNumber =33;
-let stringNumber = String(someNumber);
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log("----------------------------");
+
+// let isLoggedIn =0;
+// let booleanIsLoggedIn=Boolean(isLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+
+// // 1 => true     0=> false
+// //""  => false
+// //"Simarjit"  => true
+
+
+// console.log("-----------------------");
+
+// let someNumber =33;
+// let stringNumber = String(someNumber);
+// console.log(typeof stringNumber);
+//console.log(stringNumber);
+
+// *****************8 operations ****************8
+let value=5;
+let negValue=-value;
+//console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 ** 3);
+// console.log(5 % 2);
+
+// let str1= "Hello ";
+// let str2 = "Simarjit";
+// let str3= str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + "2" + 2);
+// console.log(1 + 2 + "2");
+
+console.log(true);
+console.log(+true);
+console.log(-true);
+console.log(+"");
