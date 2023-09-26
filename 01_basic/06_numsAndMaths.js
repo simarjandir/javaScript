3eff5d58cb8c9a98408e639342b5a10062ccebd8
simarjandir <145894355+simarjandir@users.noreply.github.com>
2023-09-26 16:25:28 +0000

    Num And Maths done

---
+++ 01_basic/06_numsAndMaths.js
@@ -0,0 +1,38 @@
+const num=87;
+//console.log(num);
+const bal= new Number(10);
+//console.log(bal);
+// console.log(bal.toFixed(2));
+// console.log(bal.toString());
+// console.log(bal.toString().length);
+
+// const a1=23.8966;
+// console.log(a1.toPrecision(2));
+// const a2=23;
+// console.log(a2.toPrecision(3));
+
+// const hundreds=10000000;
+// console.log(hundreds.toLocaleString()); // show comas in number as US standard
+// console.log(hundreds.toLocaleString('en-IN'));  // show comas in number as Indian standard
+
+
+// *************** MATHS *******************
+
+// console.log(Math);
+// console.log(Math.abs(-65));
+// console.log(Math.round(6.4));
+// console.log(Math.round(5.6));
+// console.log(Math.floor(5.7));
+// console.log(Math.ceil(5.1));
+//console.log(Math.random());
+//console.log((Math.random() * 10) + 1);
+//console.log(Math.floor((Math.random() * 10)) + 1);
+
+//  ################ IMPORTANT    ############ 
+const min=10;
+const max =20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+// ####################################################
+
