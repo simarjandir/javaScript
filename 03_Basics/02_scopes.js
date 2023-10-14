527f0e9af95c6ac55ed4be997c2d7ab6b854da48
simarjandir <145894355+simarjandir@users.noreply.github.com>
2023-10-14 11:18:22 +0000

    Global and local scope

---
+++ 03_Basics/02_scopes.js
@@ -0,0 +1,15 @@
+
+//var c=300;
+let a=300;
+
+if(true){
+    let a=10;
+    const b=20;
+   // var c=30;
+   console.log(`inside of function a= ${a}`);
+
+}
+
+ console.log(`outside of function a= ${a}`);
+// console.log(b);
+//console.log(c);
