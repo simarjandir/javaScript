e01d2bfdba60891946ba5351eadd1f091f554770
simarjandir <145894355+simarjandir@users.noreply.github.com>
2023-10-14 14:14:57 +0000

    this and arrow function

---
+++ 03_Basics/03_arrow.js
@@ -0,0 +1,46 @@
+// const user = {
+//     username : "simarjit",
+//     price   : 999,
+
+//     welcomeMessage  : function(){
+//         console.log(` ${this.username} , Welcome to my site`);
+//         console.log(this);
+//     }
+// }
+// user.welcomeMessage();
+
+// user.username="jot";
+// user.welcomeMessage();
+//console.log(this);
+
+// function chai(){
+//     const name="ss"
+//     //console.log(this);
+//     //console.log(this.name);
+// }
+// chai();
+
+// const chai= function(){
+//     console.log(this);
+// }
+//chai();
+
+// const chai= ()=>{
+//     console.log("hello");
+//     console.log(this);
+// }
+// chai();
+
+// const addTwo = (num1, num2) =>{
+//     return num1+num2;
+// }
+//console.log(addTwo(6,7));
+
+//const addTwo = (num1, num2) => num1 + num2;
+//console.log(addTwo(6,7));
+
+//const addTwo = (num1, num2) => (num1+num2);
+//console.log(addTwo(6,7));
+
+// const user= () => ({username : "simarjit"});
+// console.log(user());
