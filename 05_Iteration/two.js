a0dbd34e7d2cfed7ef53e867713ee36b62982a33
simarjandir <145894355+simarjandir@users.noreply.github.com>
2023-10-21 11:24:39 +0000

    loop basic

---
+++ 05_Iteration/two.js
@@ -0,0 +1,18 @@
+// let i=1;
+// while (i<=10) {
+//     console.log(` value o i= ${i}`);
+//     i+=2;
+// }
+
+let myArray=["shaktimaan","gangadhar","geeta"];
+let i=0;
+let arrayLength=myArray.length;
+// while (i<arrayLength) {
+//     console.log(myArray[i]);
+//     i++;
+// }
+
+do {
+    console.log(myArray[i]);
+    i+=1;
+} while (i<arrayLength);
