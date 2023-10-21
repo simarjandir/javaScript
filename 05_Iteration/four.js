29ef79f3e0bb160dd125c0fb8558b825f784b9f1
simarjandir <145894355+simarjandir@users.noreply.github.com>
2023-10-21 15:54:16 +0000

    for in

---
+++ 05_Iteration/four.js
@@ -0,0 +1,26 @@
+// const myObj={
+//     js : "Java Script",
+//     cpp : "C Plus Plus",
+//     swift : "Swift by apple"
+// };
+// for (const key in myObj) {
+//     console.log(` Key -->  ${key}      Value --> ${myObj[key]} `);
+// }
+
+// const programming=["JavaScript","C++","Java"];
+// for (const key in programming) {
+//     //console.log(key);
+//     console.log(` programming[${key}] = ${programming[key]}`);
+    
+// }
+
+//map
+// cannot iterate
+// let myMap=new Map();
+// myMap.set('Sun','Sunday');
+// myMap.set('Mon','Monday');
+// myMap.set('Wed','Wednesday'); 
+
+// for (const key in myMap) {
+//     console.log(key);
+// }
