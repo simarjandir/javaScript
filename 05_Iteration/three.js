8a730e70235f6875c12bfa8372913ae8fcb6a5d8
simarjandir <145894355+simarjandir@users.noreply.github.com>
2023-10-21 11:43:57 +0000

    for of and map

---
+++ 05_Iteration/three.js
@@ -0,0 +1,29 @@
+// for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+// let myArray=[1,2,3,4,5,6];
+// for (const i of myArray) {
+//     console.log(i);
+// }
+
+// let greetings="Hello word";
+// for (const greet of greetings) {
+//     if (greet == " ") {
+//         continue;
+//     }
+//     console.log(`char of greetings is ${greet}`);
+    
+// }
+
+// Map  --> contains key value pair; keys should be unique
+// let myMap=new Map();
+// myMap.set('Sun','Sunday');
+// myMap.set('Mon','Monday');
+// myMap.set('Sun','Sunday'); // will not set as key is duplicate
+// //console.log(myMap);
+// for (const [key,value] of myMap) {
+//     console.log(`${key}  --> ${value}`);
+// }
+
+
