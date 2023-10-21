f4754c14fdea96b4c8c98f30caffc4463b2808a5
simarjandir <145894355+simarjandir@users.noreply.github.com>
2023-10-21 11:09:11 +0000

    loop intro

---
+++ 05_Iteration/one.js
@@ -0,0 +1,34 @@
+// for
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     console.log(element);
+    
+// }
+// }
+
+// let myArray=["Shaktimaan","captain vyom","Aryaman"];
+// console.log(myArray.length);
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+    
+// }
+//break
+// for (let index = 0; index < 20; index++) {
+//     if (index == 5) {
+//         console.log(` Detected ${index}`);
+//         break;
+//     }
+//     console.log(`Value of index = ${index} `);
+    
+// }
+
+// continue
+
+for (let index = 0; index < 10; index++) {
+   if (index == 5) {
+    console.log(` detected ${index}`);
+    continue;
+   }
+    console.log(` Value of index is  ${index}`);
+}
