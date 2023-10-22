acef3e416a7cbf340186e4187433c5c0afe8d427
simarjandir <145894355+simarjandir@users.noreply.github.com>
2023-10-22 15:28:17 +0000

    map

--- 05_Iteration/seven.js
+++ 05_Iteration/seven.js
@@ -1,5 +1,10 @@
 let myNums=[1,2,3,4,5,6,7,8,9,10];
 //map
 
-const newNums= myNums.map((num) => num+10);
+//const newNums= myNums.map((num) => num+10);
+
+const newNums=myNums
+            .map((num) => num*10)
+            .map((num) => num+1)
+            .filter((num) => num >=50)
 console.log(newNums);
